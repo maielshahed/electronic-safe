35a12f6d2386380a81aa3a68a2f287b6119dff17
maielshahed <andromeda_m29@yahoo.com>
2024-07-29 11:36:25 +0200

    first commit

---
+++ safe_using_keypad/Debug/makedep.mk
@@ -0,0 +1,16 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+Dio.c
+
+eeprom_driver.c
+
+keypad_driver.c
+
+LCD.c
+
+safe_using_keypad.c
+
+timer.c
+
